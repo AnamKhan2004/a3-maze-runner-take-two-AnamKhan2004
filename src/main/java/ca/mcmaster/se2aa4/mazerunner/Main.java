53849f60d2d5f9fbeccd90a10ba40841099ae947
Anam <khana489@AnamThinkpad>
2024-01-23 23:23:44 -0500

    Walking Skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,8 +3,6 @@ package ca.mcmaster.se2aa4.mazerunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.lang.module.Configuration;
-
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
