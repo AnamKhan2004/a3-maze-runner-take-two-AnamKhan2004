ca02bb6ce9fb52298a3fc907ff072b36cdb32bf5
Anam <khana489@AnamThinkpad>
2024-01-24 21:08:13 -0500

    reads file and makes array, finds entry and exit points (close #3)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Maze {
     private static final Logger logger = LogManager.getLogger();
@@ -14,26 +15,30 @@ public class Maze {
     public boolean checkedPath;
 
     public Maze(String inputFile) throws IOException {
-        int[][] mazeArr = readMaze(inputFile);
+        ArrayList<ArrayList<Integer>> mazeArr = readMaze(inputFile);
         Path path = new Path(mazeArr);
         this.generatedPath = path.generatedPath;
         this.checkedPath = path.checkedPath;
     }
 
-    private static int[][] readMaze(String inputFile) throws IOException {
+    private static ArrayList<ArrayList<Integer>> readMaze(String inputFile) throws IOException {
+        ArrayList<ArrayList<Integer>> mazeList = new ArrayList<>();
+
         BufferedReader reader = new BufferedReader(new FileReader(inputFile));
         String line;
         while ((line = reader.readLine()) != null) {
+            ArrayList<Integer> row = new ArrayList<>();
             for (int idx = 0; idx < line.length(); idx++) {
                 if (line.charAt(idx) == '#') {
-                    System.out.println("WALL ");
+                    row.add(1);
                 } else if (line.charAt(idx) == ' ') {
-                    System.out.println("PASS ");
+                    row.add(0);
                 }
             }
-            logger.info(System.lineSeparator());
+            mazeList.add(row);
         }
 
-        return new int[][]{{1,0},{1,0}};
+        System.out.println(mazeList);
+        return mazeList;
     }
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -1,20 +1,47 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Path {
 
     public String generatedPath;
     public boolean checkedPath;
 
-    public Path(int[][] mazeArr) {
+    public Path(ArrayList<ArrayList<Integer>> mazeArr) {
         this.generatedPath = generatePath(mazeArr);
         this.checkedPath = checkPath(mazeArr);
     }
 
-    private static String generatePath(int[][] mazeArray){
+    private static String generatePath(ArrayList<ArrayList<Integer>> mazeArray){
+        int numRows = mazeArray.size();
+        int numCols = mazeArray.get(0).size();
+
+        int[] entry = new int[2];
+        entry[0] = 0;
+        int[] exit = new int[2];
+        exit[0] = numCols - 1;
+
+        for (int i = 0; i < numRows; i++) {
+            if (mazeArray.get(i).get(0) == 0) {
+                entry[1] = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < numRows; i++) {
+            if ((mazeArray.get(i).get(numCols - 1)) == 0) {
+                exit[1] = i;
+                break;
+            }
+        }
+
+        System.out.println(Arrays.toString(entry));
+        System.out.println(Arrays.toString(exit));
         return "FFLLRR";
     }
 
-    private static boolean checkPath(int[][] mazeArray){
+    private static boolean checkPath(ArrayList<ArrayList<Integer>> mazeArray){
         return true;
     }
 }
